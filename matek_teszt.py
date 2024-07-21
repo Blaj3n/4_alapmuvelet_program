150f616d72de29af9209f3e1e457dc0e5596bec2
Blaj3n <benceulrich@gmail.com>
2024-07-21 12:00:56 +0200

    1-nél kisebb 4-nél nagyobb értékek kezelése HIBA

--- matek_teszt.py
+++ matek_teszt.py
@@ -1,23 +1,17 @@
+# átlag/százalékszámítás, kerekítés
+# illetve felhasználó által megadott feladatok kiírása pluszba
+# válasz CSAK 1 és 3 közötti szám lehet -->  HIBA!! ha 1-nél kisebb vagy 4-nél nagyobb számot adunk meg hibára fut
 import random
-# from random import randint <- ez azt jelenti, hogy a random osztályBÓL (from) importáljuk a "randint" metódust.
 
-# print("1. feladat")
+print(f"Milyen műveletet szeretnél gyakorolni? \n\n\t1. Összeadás \n\t2. Kivonás \n\t3. Szorzás \n\t4. Osztás")
 
-print(f"Milyen műveletet szeretne gyakorolni? \n\n\t1. Összeadás \n\t2. Kivonás \n\t3. Szorzás \n\t4. Osztás")
 
-# print("2. feladat")
+valasz = int(input("\nVálasztás (1-4): "))
 
-valasz = int(input("\nVálasztás (1-3): ")) # "3" <- stringként menti el, és ezt kell intté (egész számmá alakítani.) Jegyezd meg: int(input())
-
-# print("3. feladat")
 
 # db = int(input("Hány műveletet szeretnél?: "))
-db = 0 # <- ez alapvetően int, ha meg macskaköröm közé van ékelve, akkor alapvetően string. "0"
+db = 0
 ok = 0
-
-# print("4. feladat"), print("5. feladat")
-
-# a feltételes ciklus mindig a while ciklus! - amíg, (amíg valami igaz, addig fusson, pl.: amíg i = 3, addig fusson, amint i nem 3, álljon le. Tehát a while ciklus majdnem olyan, mint a for ciklus, csak "break"-kel.)
 
 while ok < 5:
     db += 1
@@ -30,23 +24,22 @@ while ok < 5:
     elif valasz == 2:
         d = (a - b)
         c = int(input(f"{a} - {b} = "))
-    else:
+    elif valasz == 3:
         d = (a * b)
         c = int(input(f"{a} * {b} = "))
-
+    elif valasz == 4:
+        d = (a / b)
+        c = int(input(f"{a} / {b} = "))
+    else:
+        print("Rossz számot adtál meg!")
+        valasz = int(input("\nVálasztás (1-4): "))
     if c == d:
         ok += 1
         print("Helyes! ")
     else:
         print("Hibás! ")
 
-# print("6. feladat")
-
 print(f"Gratulálunk!\nSikerült 5 helyes műveletet elvégezni {db} próbálkozásból. ", end="")
 
-# átlag/százalékszámítás, kerekítés
-# illetve felhasználó által megadott feladat kiírása pluszba
-# válasz CSAK 1 és 3 közötti szám lehet
-# 4. opció: OSZTÁS
 
-print(f"{round((ok/db) * 100, 2)}%") # <- round = kerekítés
+print(f"{round((ok/db) * 100, 2)}%")
